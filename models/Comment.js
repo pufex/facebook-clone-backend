a31a2b7a30584821f308debb044ebca69e7882af
Jakub Abram <j.abram1@wp.pl>
2025-12-01 11:09:26 +0100

    Created Comment Model

---
+++ models/Comment.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose"
+
+const CommentSchema = new mongoose.Schema({
+    user_id: {
+        type: String,
+        required: true,
+    },
+    body: {
+        type: String,
+        required: true,
+    }, 
+    pictures_ids: {
+        type: [String],
+        default: []
+    },
+    liked_by: {
+        type: [String],
+        default: []
+    }
+}, {timestamps: true})
+
+const Comment = mongoose.model("Comment", CommentSchema)
+
+export default Comment
