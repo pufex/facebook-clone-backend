a63ef4d14d8806a95846565340fa97b27d6a6984
Jakub Abram <j.abram1@wp.pl>
2025-12-01 11:45:07 +0100

    Added useful middlewares to the express application

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 import express from "express"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import bodyParser from "body-parser"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import morgan from "morgan"
+import credentials from "./middleware/credentials.js"
 
 dotenv.config()
 
 const app = express()
+
+app.use(morgan("dev"))
+app.use(credentials)
+app.use(cors({origin: "http://localhost:3003"}))
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 mongoose.connect(process.env.MONGODB_URI)
     .then(() => {
