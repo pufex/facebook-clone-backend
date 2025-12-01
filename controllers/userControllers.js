d2b801be3a81d3c5e70b0f2c97261de7fbb6eca3
Jakub Abram <j.abram1@wp.pl>
2025-12-01 17:02:46 +0100

    Created controllers for setting profile and background pictures

---
+++ controllers/userControllers.js
@@ -0,0 +1,46 @@
+import User from "../models/User";
+import ImageDeclaration from "../models/ImageDeclaration";
+
+export const setProfilePicture = async (req,res) => {
+    const user_id = req.user._id
+    const declaration_id = req.params.id
+    try{
+        const declaration = await ImageDeclaration.findById(user_id)
+        if(!declaration){
+            return res.sendStatus(404)
+        }
+        if(declaration.user_id !== user_id){
+            return res.sendStatus(403)
+        }
+        await User.findByIdAndUpdate(
+            user_id, 
+            { profile_picture_id: declaration_id }
+        )
+        res.json(declaration)
+    }catch(err){
+        console.log(err)
+        res.sendStatus(500)
+    }
+}
+
+export const setBackgroundPicture = async (req, res) => {
+    const declaration_id = req.params._id
+    const user_id = req.user._id
+    try{
+        const declaration = await ImageDeclaration.findById(declaration_id)
+        if(!declaration){
+            return res.sendStatus(404)
+        }
+        if(declaration.user_id !== user_id){
+            return res.sendStatus(403)
+        }
+        await User.findByIdAndUpdate(
+            user_id, 
+            { background_picture_id: declaration_id }
+        )
+        res.json(declaration)
+    }catch(err){
+        console.log(err)
+        res.sendStatus(500)
+    }
+}
